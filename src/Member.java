dd5b5c2cf0a7852666415010c617b7e5f68410f1
Pranav Burugula <pranav.burugula@gmail.com>
2018-10-25 12:35:31 -0400

    initial

---
+++ src/Member.java
@@ -0,0 +1,57 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Member {
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    private String fName;
+    private String lName;
+    private SimpleDateFormat timeFormat;
+    private SimpleDateFormat dateFormat;
+    private Date date;
+    private Date time;
+    private long fullInTime;
+    private long fullOutTime;
+    private int[] formalInTime;
+    private int[] formalOutTime;
+
+    public long getDifference() {
+        return difference;
+    }
+
+    private long difference;
+
+
+    public Member(String fName, String lName){
+        this.fName = fName;
+        this.lName = lName;
+        Calendar calendar = new GregorianCalendar();
+        timeFormat = new SimpleDateFormat("HH:mm:ss");
+        dateFormat = new SimpleDateFormat("MM-dd-yy");
+
+        formalInTime = new int[]{calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND)};
+        fullInTime = calendar.getTimeInMillis();
+        System.out.println(fullInTime);
+    }
+
+    public void signOut(){
+        Calendar calendar = new GregorianCalendar();
+        formalOutTime = new int[]{calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND)};
+        fullOutTime = calendar.getTimeInMillis();
+        difference = (fullOutTime-fullInTime)/60000;
+        System.out.println(fullOutTime);
+    }
+
+    public String getSaveOutput(){
+        String line = fName + "," + lName + "," + formalInTime[0]+":"+formalInTime[1]+":"+formalInTime[2] + "," + formalOutTime[0]+":"+formalOutTime[1]+":"+formalOutTime[2] + "," + difference +"\n";
+        return line;
+    }
+}
---
+++ src/MemberListReader.java
@@ -0,0 +1,117 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class MemberListReader {
+    private BufferedReader reader;
+    private BufferedWriter attendanceWriter;
+    private File rosterFile;
+    private Calendar calendar;
+
+    public ArrayList<String> getNames() {
+        return names;
+    }
+
+    private ArrayList<String> names;
+
+    public MemberListReader(String filename){
+        calendar = new GregorianCalendar();
+        rosterFile = new File(filename);
+        names = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(rosterFile));
+            String line = reader.readLine();
+            line = reader.readLine();
+            while(line != null){
+                names.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean findName(ArrayList<String> arr, String f, String l){
+        String[] fNamesArr = new String[arr.size()];
+        String[] lNamesArr = new String[arr.size()];
+        for(int i=0;i<fNamesArr.length;i++){
+            String[] temp = arr.get(i).split(",");
+            fNamesArr[i] = temp[0];
+            lNamesArr[i] = temp[1];
+        }
+//        int fIndex = binarySearch(fNamesArr, 0, fNamesArr.length-1, f);
+//        int lIndex = binarySearch(lNamesArr, 0, lNamesArr.length-1, l);
+        int fIndex = nameSearch(fNamesArr, f);
+        int lIndex = nameSearch(lNamesArr, l);
+
+        if(fIndex<0 || lIndex<0){
+            System.out.println(f+" "+l+" not found");
+            return false;
+        }
+        System.out.println(f+" "+l+" found");
+        return true;
+    }
+
+    public void saveMembers(ArrayList<Member> members){
+        try {
+            File file = new File("attendance\\attendance"+calendar.get(Calendar.MONTH)+"-"+calendar.get(Calendar.DATE)+"-"+calendar.get(Calendar.YEAR)+".csv");
+            attendanceWriter = new BufferedWriter(new FileWriter(file));
+            for (Member m : members) {
+                String line = m.getSaveOutput();
+                attendanceWriter.write(line);
+            }
+            attendanceWriter.flush();
+            attendanceWriter.close();
+
+            String[] fMembersArr = new String[members.size()];
+            String[] lMembersArr = new String[members.size()];
+            for(int i=0;i<fMembersArr.length;i++){
+                fMembersArr[i] = members.get(i).getfName();
+                lMembersArr[i] = members.get(i).getlName();
+            }
+            String[] fNamesArr = new String[names.size()];
+            String[] lNamesArr = new String[names.size()];
+            for(int i=0;i<fNamesArr.length;i++){
+                String[] temp = names.get(i).split(",");
+                fNamesArr[i] = temp[0];
+                lNamesArr[i] = temp[1];
+            }
+            for (int i=0;i<fNamesArr.length;i++){
+                int fIndex = nameSearch(fMembersArr, fNamesArr[i]);
+                int lIndex = nameSearch(lMembersArr, lNamesArr[i]);
+                String[] arr = names.get(i).split(",");
+                if (fIndex>=0 && lIndex>=0){
+                    arr[2]=""+(Integer.parseInt(arr[2])+1);
+                    long difference = members.get(fIndex).getDifference();
+                    arr[4]=""+(Long.parseLong(arr[4])+difference);
+                }
+                else {
+                    arr[3]=""+(Integer.parseInt(arr[3])+1);
+                }
+                String line = arr[0]+","+arr[1]+","+arr[2]+","+arr[3]+"\n";
+                names.set(i, line);
+            }
+            BufferedWriter rosterWriter = new BufferedWriter(new FileWriter(rosterFile));
+            for (String n : names){
+                rosterWriter.write(n);
+            }
+            rosterWriter.flush();
+            rosterWriter.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private int nameSearch(String[] arr, String target){
+        for(int i=0;i<arr.length;i++){
+            if (arr[i].equalsIgnoreCase(target)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
